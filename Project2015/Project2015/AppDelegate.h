63a494506db05d5802c066182de7fbacad8a0fb7
john.wei <john@johnweideiMac.local>
2015-06-23 16:25:49 +0800

    create project

---
+++ Project2015/Project2015/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Project2015
+//
+//  Created by john.wei on 15/6/23.
+//  Copyright (c) 2015年 whj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Project2015/Project2015/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Project2015
+//
+//  Created by john.wei on 15/6/23.
+//  Copyright (c) 2015年 whj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
